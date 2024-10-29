5faae0e934ff48410248b4547e5de05410dcd6c8
fbansept <bansept.franck@gmail.com>
2024-10-29 10:39:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "tier_list_dfs_24",
+});
+
+app.get("/test", (req, res) => {
+  connection.connect();
+
+  connection.query(
+    ` SELECT url, i.id AS id_image, id_categorie, nom
+    FROM image i
+    JOIN categorie c ON i.id_categorie = c.id
+    WHERE c.id_utilisateur = 1`,
+    (err, rows) => {
+      
+      connection.end();
+
+      if (err) throw err;
+
+      res.send(JSON.stringify(rows));
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port} !!!!!!`);
+});
