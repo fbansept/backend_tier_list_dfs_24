c2c3e6aed0dd6dfec149bfca47acfc4410f09dbe
fbansept <bansept.franck@gmail.com>
2024-10-30 11:46:30 +0100

    add middleware

--- index.js
+++ index.js
@@ -17,6 +17,27 @@ const connection = mysql.createConnection({
   connectionLimit: 10,
 });
 
+function authenticateToken(req, res, next) {
+
+  //on recupere le jwt dans le header authorization
+  const jwt_utilisateur = req.headers.authorization;
+
+  //si il n'y a pas d'entete authorization on bloque la requete
+  if (!jwt_utilisateur) {
+    res.status(403).send();
+  } else {
+    //on vérifie que le JWT est valide
+    jwt.verify(jwt_utilisateur, secret_jwt, (err, donnees_jwt) => {
+      //si la signature ne correspond pas ou si le jwt est invalide
+      if (err) return res.sendStatus(403);
+
+      req.user = donnees_jwt;
+
+      next();
+    });
+  }
+}
+
 app.post("/login", (req, res) => {
   const utilisateur = req.body;
 
@@ -41,90 +62,40 @@ app.post("/login", (req, res) => {
   );
 });
 
-app.get("/categories", (req, res) => {
-  //on recupere le jwt dans le header authorization
-  const jwt_utilisateur = req.headers.authorization;
-
-  //si il n'y a pas d'entete authorization on bloque la requete
-  if (!jwt_utilisateur) {
-    res.status(403).send();
-  } else {
-    //on vérifie que le JWT est valide
-    jwt.verify(jwt_utilisateur, secret_jwt, (err, donnees_jwt) => {
-      //si la signature ne correspond pas ou si le jwt est invalide
-      if (err) return res.sendStatus(403);
-
-      const id_utilisateur = donnees_jwt.sub;
-
-      connection.execute(
-        ` SELECT url, i.id AS id_image, c.id as id_categorie, nom AS nom_categorie
+app.get("/categories", authenticateToken, (req, res) => {
+  connection.execute(
+    ` SELECT url, i.id AS id_image, c.id as id_categorie, nom AS nom_categorie
           FROM image i
           RIGHT JOIN categorie c ON i.id_categorie = c.id
           WHERE c.id_utilisateur = ?`,
-        [id_utilisateur],
-        (err, rows) => {
-          if (err) throw err;
+    [req.user.sub],
+    (err, rows) => {
+      if (err) throw err;
 
-          // //solution 1 :
+      const categories = rows.reduce((accumulateur, image) => {
+        //si on trouve la catégorie on ajoute l'image à cette c&tégorie,
+        //sinon on ajoute un nouvel objet au tableau
 
-          // //on réalise une structure différente du résultat :
-          // // un tableau avec des objet possédant le nom de la catégorie et les images liées
-          // const groupeParCategorie = {};
+        const categorieExistante = accumulateur.filter(
+          (categorie) => categorie.nom == image.nom_categorie
+        );
 
-          // //on parcours toutes les lignes du résultat de la requete (qui contienneny chaque images)
-          // for (let image of rows) {
-          //   //si la catégorie de l'image n'a pas encore été ajoutée, on l'ajoute,
-          //   // sinon on ajoute seulement l'image dans sa liste d'images
-          //   if (groupeParCategorie[image.nom_categorie]) {
-          //     groupeParCategorie[image.nom_categorie].images.push(image.url);
-          //   } else {
-          //     groupeParCategorie[image.nom_categorie] = {
-          //       id: image.id_categorie,
-          //       nom: image.nom_categorie,
-          //       images: image.url ? [image.url] : [],
-          //     };
-          //   }
-          // }
-
-          // //on transforme en tableau l'objet qui nous a servis
-          // // à regrouper les images par catégorie
-          // const categories = [];
-
-          // //on parcours tous les noms des propriétés de l'objet qui regroupe les images par catégiorie
-          // for (let nomCategorie in groupeParCategorie) {
-          //   categories.push(groupeParCategorie[nomCategorie]);
-          // }
-
-          // res.send(JSON.stringify(categories));
-
-          //solution 2 :
-
-          const categories = rows.reduce((accumulateur, image) => {
-            //si on trouve la catégorie on ajoute l'image à cette c&tégorie,
-            //sinon on ajoute un nouvel objet au tableau
-
-            const categorieExistante = accumulateur.filter(
-              (categorie) => categorie.nom == image.nom_categorie
-            );
-
-            if (categorieExistante.length >= 1) {
-              categorieExistante[0].images.push(image.url);
-            } else {
-              accumulateur.push({
-                nom: image.nom_categorie,
-                id: image.id_categorie,
-                images: image.url ? [image.url] : [],
-              });
-            }
-
-            return accumulateur;
-          }, []);
-
-          res.send(JSON.stringify(categories));
+        if (categorieExistante.length >= 1) {
+          categorieExistante[0].images.push(image.url);
+        } else {
+          accumulateur.push({
+            nom: image.nom_categorie,
+            id: image.id_categorie,
+            images: image.url ? [image.url] : [],
+          });
         }
-      );
-    });
-  }
+
+        return accumulateur;
+      }, []);
+
+      res.send(JSON.stringify(categories));
+    }
+  );
 });
 
 app.listen(port, () => {
